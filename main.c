1052c0664c54a0a5373d6634880d02499e31d50e
Vinuki Hasithma <hasithmavinuki@gmail.com>
2026-04-01 15:24:34 +0530

    add some new features

--- main.c
+++ main.c
@@ -6,21 +6,53 @@
 
 
 int main(void){
-   srand(time(NULL));
-   int random_num = (rand() % 100 ) + 1;
-   bool answer = false ;
+   
+   
    int user_num = 0 ;
-   int count = 0 ;
+   int level = 0 ;
+   int max = 0 ;
+   char play_or_not = '\0' ;
 
 
-   while (answer == false){
-      printf("\nGuess a number. (1 - 100)  ");
-      scanf("%d" , &user_num) ;
+   do{bool answer = false ;
+      int count = 0 ;
+
+      printf("Choose difficulty:\n");
+      printf("1. Easy   (1 - 50)\n");
+      printf("2. Medium (1 - 100)\n");
+      printf("3. Hard   (1 - 200)\nEnter the level: ");
+      scanf("%d" , &level) ;
+      switch(level){
+         case 1 :
+            max = 50 ;
+            break ;
+         case 2 :
+            max = 100 ;
+            break ;
+         case 3 :
+            max = 200 ;
+            break ;
+
+      }
+      srand(time(NULL));
+      int random_num = (rand() % max ) + 1;
+
+      while (answer == false){
+      printf("Guess the number: ");
+      // scanf("%d" , &user_num) ;
+      if  (scanf("%d" , &user_num) != 1){
+         printf("Invalid input !");
+         while(getchar() != '\n');
+         continue ;
+         
+      }
       count ++ ;
 
       if (user_num == random_num){
          printf("Your guess is correct !! You got it in %d tries.\n" , count);
          answer = true ;
+         printf("Do you want to play again? Y OR N ");
+         scanf(" %c" , &play_or_not);
       }
       else if (user_num < random_num){
          printf("It's low. try a higher number.\n");
@@ -29,13 +61,16 @@ int main(void){
       else if(user_num > random_num){
          printf("It's high. try a lower number.\n");
       }
-      
-      else {
-         printf("Invalid input ! Try again.\n");
       }
+   
 
 
-   }
+   
+      
+      
+
+
+   } while (play_or_not == 'y' || play_or_not == 'Y') ;
       
    return 0 ;
 }
